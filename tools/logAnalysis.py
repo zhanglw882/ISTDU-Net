2466551bf7efc77dfbe57838506576fdc1cb814c
zhanglw882 <49709900+zhanglw882@users.noreply.github.com>
2023-11-25 11:11:35 +0800

    Add files via upload

--- tools/logAnalysis.py
+++ tools/logAnalysis.py
@@ -1,1 +1,16 @@
+# import
 
+
+# srcFile = './logCtea.txt'
+# srcFile = './logCtd.txt'
+srcFile = './logCt.txt'
+sf = open(srcFile, 'r')
+aucs = []
+for line in sf:
+    if 'val_metrics' not in line:
+        continue
+    # print(line)
+    auc = line.split(':')[-1]
+    aucs.append(float(auc))
+sf.close()
+print(max(aucs))
